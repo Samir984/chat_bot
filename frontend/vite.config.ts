401e5c1b7ac98a8324738d931768d5b59a417c44
Samir Neupane <sasuki984@gmail.com>
2025-11-23 22:23:53 +0545

    setup: codeInspectorPlugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,25 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    ...(mode === "development"
+      ? [
+          codeInspectorPlugin({
+            bundler: "vite",
+          }),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
